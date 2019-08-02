72a7cd8246c0b1631b7356526c059dd628566ca4
Kirk Kaiser <kirk@zothcorp.com>
2019-08-02 15:27:42 -0400

    Switch to f-strings for parameters

--- plantadog.py
+++ plantadog.py
@@ -21,21 +21,21 @@ while True:
     # get number of hosts running
     total_hosts = api.Hosts.totals()
     print(total_hosts['total_up'])
-    teensy.write(bytes('@T' + str(total_hosts['total_up']), 'ascii'))
+    teensy.write(bytes(f"@T{total_hosts['total_up']:02}", 'ascii'))
 
     # get if any events have occurred in past 10 mins
     end_time = time.time()
     start_time = end_time - (60 * 10)
     events = api.Event.query(start=start_time,
                              end=end_time)
-    teensy.write(bytes('@E' + str(len(events['events'])), 'ascii'))
+    teensy.write(bytes(f"@E{len(events['events']):02}", 'ascii'))
 
     # see if our monitor triggered
     mon = api.Monitor.get(10780655, group_states='alert')
     print(mon['overall_state'])
     if mon['overall_state'] == 'OK':
-        teensy.write(bytes('@M1', 'ascii'))
+        teensy.write(bytes('@M01', 'ascii'))
     else:
-        teensy.write(bytes('@M0', 'ascii'))
+        teensy.write(bytes('@M00', 'ascii'))
     time.sleep(5)
     print("Looping... ")
