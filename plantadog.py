f2235e291621f9d051f434511bb7ec6793ddc8e5
Kirk Kaiser <kirk@zothcorp.com>
2019-08-02 14:25:48 -0400

    Add Datadog API

--- plantadog.py
+++ plantadog.py
@@ -1,14 +1,23 @@
 import serial
 import time
 
+from datadog import initialize, api
+
+# reads environment variables for
+# DD_API_KEY and DD_APP_KEY
+initialize()
+
+# initialize serial port... you'll probably change this
 port = '/dev/tty.usbmodem54572501'
 teensy = serial.Serial(port, 9600, timeout=5)
 
 while True:
+    # first, see if the teensy has said anything
     from_teensy = teensy.read_all()
     if from_teensy:
         print(from_teensy)
     time.sleep(100)
     # to write to teensy...
+    # will do for an event posted to DD
     # teensy.write(bytes("test", ascii))
 
